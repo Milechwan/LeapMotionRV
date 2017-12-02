d7d25c50b38cd8453c0dd255dde01a86120a3efd
unknown <mscc@cin.ufpe.br>
2017-12-02 17:45:22 -0300

    script para pegar valores entre cenas a ser concluido

---
+++ LeapMotion/Assets/comecarExercicio.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class comecarExercicio : MonoBehaviour {
+    public Button comecar;
+ 
+    // Use this for initialization
+    void Start () {
+        Button btn = comecar.GetComponent<Button>();
+        btn.onClick.AddListener(exercicio);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    void exercicio()
+    {
+        SceneManager.LoadScene("cena_exercicios");
+    }
+}
---
+++ LeapMotion/Assets/pegaTexto.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pegaTexto : MonoBehaviour {
+    public InputField campoAbdu;
+    static string teste; 
+	// Use this for initialization
+	void Start () {
+        teste = campoAbdu.textComponent.text; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(teste);
+	}
+}
